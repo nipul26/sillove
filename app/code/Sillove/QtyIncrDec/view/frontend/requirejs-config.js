2a3cc2a386ee44d0b38746ecd4ca9a842087193c
Vrunda Barochiya <vrunda@Vrundas-MacBook-Air-2.local>
2025-09-18 00:54:52 +0530

    Add Qty Increment-Decrement Buttons

---
+++ app/code/Sillove/QtyIncrDec/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'qty-counter': 'Sillove_QtyIncrDec/js/qty-counter',
+            'cartQtyUpdate': 'Sillove_QtyIncrDec/js/cartQtyUpdate'
+        }
+    }
+};
