fc722656345c36d6216677b2e4e3b58eaaa4f7a9
niravkumbhariya <niravkumbhariya101@gmail.com>
2025-12-12 11:44:04 +0530

    design changes

---
+++ app/design/frontend/Sillove/Elegant/Magento_LayeredNavigation/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_LayeredNavigation/js/accordion': {
+                'Magento_Theme/js/disable-accordion': true
+            }
+        }
+    }
+};
