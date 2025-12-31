872a1671aff8478482021620927527e6c1969aae
niravkumbhariya <niravkumbhariya101@gmail.com>
2025-12-31 20:18:30 +0530

    product details page design changes

---
+++ app/design/frontend/Sillove/Elegant/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    map: {
+        '*': {
+            customOwl: 'js/custom-owl'
+        }
+    },
+    paths: {
+        'owlcarousel': 'owlcarousel/owl.carousel.min'
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};
