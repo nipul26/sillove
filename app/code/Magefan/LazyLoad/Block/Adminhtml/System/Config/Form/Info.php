c48cfac00eab1e02201a71e683f303244eb746ca
nipulthoriya <nipulprajapati325@gmail.com>
2026-03-11 22:04:53 +0530

    Lazyload module

---
+++ app/code/Magefan/LazyLoad/Block/Adminhtml/System/Config/Form/Info.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\LazyLoad\Block\Adminhtml\System\Config\Form;
+
+/**
+ * Admin blog configurations information block
+ */
+class Info extends \Magefan\Community\Block\Adminhtml\System\Config\Form\Info
+{
+    /**
+     * Return extension url
+     * @return string
+     */
+    protected function getModuleUrl()
+    {
+        return 'https://mage' . 'fan.com/magento-2-image-lazy-load-extension';
+    }
+
+    /**
+     * Return extension title
+     * @return string
+     */
+    protected function getModuleTitle()
+    {
+        return 'Image Lazy Load Extension';
+    }
+}
---
+++ app/code/Magefan/LazyLoad/Block/Lazy.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\LazyLoad\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magefan\LazyLoad\Model\Config;
+
+/**
+ * Init lazy load
+ */
+class Lazy extends Template
+{
+    /**
+     * @param Context $context
+     * @param Config $config
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Config $config,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->config = $config;
+    }
+
+    /**
+     * Retrieve block html
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if ($this->config->getEnabled()) {
+            return parent::_toHtml();
+        }
+
+        return '';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNoScriptEnabled(): bool
+    {
+        return (bool)$this->config->isNoScriptEnabled();
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsJavascriptLazyLoadMethod(): bool
+    {
+        return $this->config->getIsJavascriptLazyLoadMethod();
+    }
+
+    /**
+     * Retrieve lazy load config json string
+     *
+     * @return string
+     */
+    public function getLazyLoadConfig(): string
+    {
+        $config = $this->getData('lazy_load_config');
+
+        if (!is_array($config)) {
+            $config = [];
+        }
+
+        if (!isset($config['elements_selector'])) {
+            $config['elements_selector'] = 'img,div';
+        }
+
+        if (!isset($config['data_srcset'])) {
+            $config['data_srcset'] = 'originalset';
+        }
+
+        return json_encode($config);
+    }
+}
