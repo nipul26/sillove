bca56d61090805a50fe9567c35737905c0ff3203
nipulthoriya <nipulprajapati325@gmail.com>
2026-04-07 21:27:26 +0530

    Added files for the discount persantage

---
+++ app/code/Magecomp/Discountpercentage/Plugin/CartItemPrice.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Magecomp\Discountpercentage\Plugin;
+
+use Magecomp\Discountpercentage\Helper\Data as DiscountHelper;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Quote\Model\Quote\Item\AbstractItem;
+
+class CartItemPrice
+{
+    /**
+     * @var DiscountHelper
+     */
+    private $discountHelper;
+
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
+
+    public function __construct(
+        DiscountHelper $discountHelper,
+        PriceCurrencyInterface $priceCurrency
+    ) {
+        $this->discountHelper = $discountHelper;
+        $this->priceCurrency = $priceCurrency;
+    }
+
+    public function afterGetUnitPriceHtml(
+        \Magento\Checkout\Block\Cart\Item\Renderer $subject,
+        string $result,
+        AbstractItem $item
+    ): string {
+        if (!$this->discountHelper->isActive()) {
+            return $result;
+        }
+
+        $product = $item->getProduct();
+        $regularPrice = (float)$product->getPriceInfo()->getPrice('regular_price')->getAmount()->getValue();
+        $finalPrice = (float)$product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
+
+        if ($regularPrice <= 0 || $finalPrice <= 0 || $finalPrice >= $regularPrice) {
+            return $result;
+        }
+
+        $discountPercent = (int)round((($regularPrice - $finalPrice) / $regularPrice) * 100);
+        $formattedRegularPrice = $this->priceCurrency->format($regularPrice, false, 2);
+
+        $discountHtml = '<div class="discount-info" style="margin-top:4px;">'
+            . '<span style="color:#008000; font-weight:bold; font-size:13px;">'
+            . $discountPercent . '% ' . __('off')
+            . '</span>'
+            . ' <span style="text-decoration:line-through; color:#999; font-size:13px;">'
+            . $formattedRegularPrice
+            . '</span>'
+            . '</div>';
+
+        return $result . $discountHtml;
+    }
+}
