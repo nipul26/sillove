f7fb402c3a59b4c224f65236d63bb00b30d1e386
root <root@ubuntu-cloudpanel.localhost>
2026-06-18 06:59:33 +0000

    Added files for the mobile otp

--- app/code/Sillove/MobileOtp/Setup/Patch/Data/AddMobileNumberAttribute.php
+++ app/code/Sillove/MobileOtp/Setup/Patch/Data/AddMobileNumberAttribute.php
@@ -1,62 +1,95 @@
 <?php
 namespace Sillove\MobileOtp\Setup\Patch\Data;
 
-use Magento\Customer\Model\Customer;
-use Magento\Customer\Setup\CustomerSetupFactory;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
 
 class AddMobileNumberAttribute implements DataPatchInterface
 {
+    /**
+     * @var ModuleDataSetupInterface
+     */
     private $moduleDataSetup;
-    private $customerSetupFactory;
 
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
-        CustomerSetupFactory $customerSetupFactory
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
     }
 
+    /**
+     * Add eav attributes
+     */
     public function apply()
     {
+        /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
 
-        $customerSetup->addAttribute(
-            Customer::ENTITY,
-            'mobile_number',
-            [
-                'type' => 'varchar',
-                'label' => 'Mobile Number',
-                'input' => 'text',
-                'required' => false,
-                'visible' => true,
-                'user_defined' => true,
-                'position' => 100,
-                'system' => 0,
-                'is_used_in_grid' => true,
-                'is_visible_in_grid' => true,
-                'is_filterable_in_grid' => true,
-                'is_searchable_in_grid' => true,
-            ]
-        );
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'mobile_number');
-        $attribute->setData(
-            'used_in_forms',
-            ['adminhtml_customer', 'customer_account_create', 'customer_account_edit', 'checkout_register']
-        );
+        /** @var $attributeSet AttributeSet */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $customerSetup->addAttribute(Customer::ENTITY, 'mobile_number', [
+            'type' => 'varchar',
+            'label' => 'Mobile Number',
+            'input' => 'text',
+            'required' => false,
+            'visible' => true,
+            'user_defined' => 1,
+            'is_used_in_grid' => 1,
+            'is_visible_in_grid' => 1,
+            'is_filterable_in_grid' => 1,
+            'is_searchable_in_grid' => 1,
+            'position' => 999,
+            'system' => 0,
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'mobile_number')
+        ->addData([
+            'attribute_set_id' => $attributeSetId,
+            'attribute_group_id' => $attributeGroupId,
+            'used_in_forms' => ['adminhtml_customer', 'customer_account_create', 'customer_account_edit', 'checkout_register'],//you can use other forms also ['adminhtml_customer_address', 'customer_account_edit', 'customer_address_edit', 'customer_register_address', 'customer_account_create']
+        ]);
+
         $attribute->save();
-
-        return $this;
     }
 
+    /**
+     * Get dependencies
+     */
     public static function getDependencies()
     {
         return [];
     }
 
+    /**
+     * Get Aliases
+     */
     public function getAliases()
     {
         return [];
